999be06f54cfddf3fb5339fedfccb06b9977916f
Paul Fryer <paulfryer@macrowebservices.com>
2023-09-30 07:43:01 -0700

    adding infra.

---
+++ Infrastructure/Extensions.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+using Amazon.CDK;
+using Amazon.CDK.CXAPI;
+
+namespace AutoDocker
+{
+    public static class Extensions
+    {
+        public static async Task DeployAsync(this App app)
+        {
+            var cloudAssembly = app.Synth();
+
+            var stackArtifact = (CloudFormationStackArtifact)cloudAssembly.Artifacts.Single(a => a is CloudFormationStackArtifact);
+
+
+            var cloudFormationJson = File.ReadAllText(stackArtifact.TemplateFullPath);
+
+            var cloudFormation = new Amazon.CloudFormation.AmazonCloudFormationClient();
+
+            var resp = await cloudFormation.CreateStackAsync(new Amazon.CloudFormation.Model.CreateStackRequest
+            {
+                StackName = stackArtifact.StackName,
+                TemplateBody = cloudFormationJson,
+                Capabilities = new List<string> { "CAPABILITY_IAM" }
+            });
+        }
+    }
+
+    
+
+
+
+}
---
+++ Infrastructure/Program.cs
@@ -0,0 +1,124 @@
+﻿
+
+
+using Amazon.Auth.AccessControlPolicy.ActionIdentifiers;
+using Amazon.CDK;
+using Amazon.CDK.AWS.CodeBuild;
+using Amazon.CDK.AWS.CodeCommit;
+using Amazon.CDK.AWS.CodePipeline;
+using Amazon.CDK.AWS.CodePipeline.Actions;
+using Amazon.CDK.AWS.IAM;
+using Amazon.CDK.AWS.S3;
+using Amazon.CDK.CloudAssembly.Schema;
+using System.Linq;
+
+namespace AutoDocker
+{
+
+    public class AutoDockerBuildStack : Stack
+    {
+        public AutoDockerBuildStack(App scope, string id, IStackProps? props = null) : base(scope, id, props)
+        {
+
+
+            var buildProject = new PipelineProject(this, "myproject",
+                new PipelineProjectProps
+                {
+
+                    BuildSpec = BuildSpec.FromSourceFilename("buildspec.yml"),
+                    Environment = new BuildEnvironment
+                    {
+                        ComputeType = ComputeType.SMALL,
+                        Privileged = true,
+                        BuildImage = LinuxBuildImage.FromCodeBuildImageId("aws/codebuild/standard:7.0")
+                    }
+                });
+
+            var codeCommit = new Repository(this, "myrepo", new RepositoryProps
+            {
+                RepositoryName = "myreponame"
+            });
+
+
+            var artifactBucket = new Bucket(this, "mycodebucket123456", new BucketProps
+            {
+                //BucketName = "somecodebucketnamehere",
+                
+                Encryption = BucketEncryption.S3_MANAGED,
+                AutoDeleteObjects = true,
+                RemovalPolicy = RemovalPolicy.DESTROY,
+              
+            });
+
+            var codePipeline = new Pipeline(this, "mycodepipeline", new PipelineProps
+            {
+                ArtifactBucket = artifactBucket,
+                Stages = new[]
+                {
+                    new Amazon.CDK.AWS.CodePipeline.StageProps
+                    {
+                        StageName = "SourceStage",
+                        
+                        Actions = new[]
+                        {
+                            new CodeCommitSourceAction(new CodeCommitSourceActionProps
+                            {
+                                ActionName = "SourceAction",
+                                Repository = codeCommit,
+                                Output = Artifact_.Artifact("SourceArtifact")
+                            })
+                        }
+                    },
+                    new Amazon.CDK.AWS.CodePipeline.StageProps
+                    {
+                        StageName = "BuildStage",
+                        Actions = new[]
+                        {
+                            new CodeBuildAction(new CodeBuildActionProps
+                            {
+                                ActionName = "BuildCodeAction",
+                                Project = buildProject,
+                                Input = Artifact_.Artifact("SourceArtifact")
+                            })
+                        }
+                    }
+                }
+            }) ;
+
+        }
+    }
+
+    public class DeployStage : Stage
+    {
+        public DeployStage(Constructs.Construct scope, string id, Amazon.CDK.IStageProps? props = null) : base(scope, id, props)
+        {
+        }
+
+        
+    }
+
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+
+
+
+
+            var app = new App();
+            var stack = new AutoDockerBuildStack(app, "myautodockerapp");
+
+
+            
+
+            await app.DeployAsync();
+
+        }
+    }
+
+    
+
+
+
+}
