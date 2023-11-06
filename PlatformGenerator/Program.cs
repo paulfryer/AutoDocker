32cc5ccb9b0f1efd57728408155bab07565cce46
Paul Fryer <paulfryer@macrowebservices.com>
2023-11-05 16:10:21 -0800

    removing platform generator  stuff.

--- PlatformGenerator/Program.cs
+++ PlatformGenerator/Program.cs
@@ -1,22 +1,20 @@
-﻿
+﻿using Amazon.CDK;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PlatformGenerator;
 
 
-
-
-
-internal class Program
+public interface IServiceConfiguration<out TService, TImplementation> where TImplementation : TService, new() 
 {
-    public static async Task Main(string[] args)
-    {
-     
+    void ConfigureServices(ServiceCollection serviceCollection);
+    
+    Stack? Infrastructure { get; }
 
+    Task CreateInfrastructure();
 
+    Task DeleteInfrastructure();
 
-
-
-    }
-
-
+    TService ServiceInstance { get; }
 
 
 }
